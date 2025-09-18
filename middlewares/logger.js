6a8ab25de6d2b72f1c052112d918548576416de9
Sreerag k b <sreeragkb7@gmail.com>
2025-09-18 10:27:44 +0530

    small changes

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,5 +1,12 @@
 const logger = (req,res,next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log(`[${new Date().toLocaleString("en-IN", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true
+})}] ${req.method} ${req.url}`);
     next();
 }
 
